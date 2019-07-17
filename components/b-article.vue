9f01c49d4df173c6499cf3f69ea26cf6a7cf2183
sometime <bing2619@163.com>
2019-07-17 23:41:18 +0800

    '添加文章卡片'

---
+++ components/b-article.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-container class="b-article pd-10">
+    <el-aside width="250px">1</el-aside>
+    <el-main>
+      <el-row>
+        <el-col>title</el-col>
+        <el-col>des</el-col>
+        <el-col>
+          <el-row>
+            <el-col>classify</el-col>
+            <el-col>time</el-col>
+            <el-col>view</el-col>
+            <el-col>like</el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'b-article',
+  data() {
+    return {
+      article: {
+        title: '',
+        des: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.b-article {
+  background-color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container ">
-    <h2>内容</h2>
+  <div class="container">
+    <b-article v-for="item in 5" :key="item"></b-article>
   </div>
 </template>
 
 <script>
+import bArticle from "@/components/b-article";
 export default {
-  layout:'blog',
+  layout: "blog",
   components: {
+    bArticle
   }
-}
+};
 </script>
 
 <style lang="less">
